07142929342b0c67e9f26f0cad5aa3b4bf2773c8
SaravanaKumar Periyasamy <speriyasamy@systemsinmotion.com>
2014-03-05 21:03:53 -0600

    chef-repo

---
+++ .chef/solo.rb
@@ -0,0 +1,6 @@
+# See http://docs.opscode.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+cookbook_path            ["#{current_dir}/../cookbooks"]
