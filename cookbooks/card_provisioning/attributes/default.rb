745a7d9871e3c624648086d6aa08ea0f8462d6ea
SaravanaKumar Periyasamy <speriyasamy@systemsinmotion.com>
2014-03-18 22:48:26 -0500

    test

--- cookbooks/card_provisioning/attributes/default.rb
+++ cookbooks/card_provisioning/attributes/default.rb
@@ -1,2 +1,2 @@
-default["cardProvisioning"]["sqs"] = "https://sqs.us-west-1.amazonaws.com/420890295988/card_generation_queue"
+default["cardProvisioning"]["sqs"] = "https://sqs.us-west-1.amazonaws.com/420890295988/card_generation_queue123456"
 default["cardProvisioning"]["rds"]  = "jdbc:mysql://bhn.cikxfglgub2u.us-west-1.rds.amazonaws.com:3306/inventorycardordermgmt"
