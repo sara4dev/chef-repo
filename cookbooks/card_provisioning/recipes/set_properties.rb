9c543d097360ebcadbdfb044e9670052732b8bca
SaravanaKumar Periyasamy <speriyasamy@systemsinmotion.com>
2014-03-27 11:03:38 -0500

    Added DB password

--- cookbooks/card_provisioning/recipes/set_properties.rb
+++ cookbooks/card_provisioning/recipes/set_properties.rb
@@ -5,7 +5,9 @@ template "/var/lib/tomcat7/webapps/cardProvisioning#v1/WEB-INF/classes/configInv
   group "tomcat7"
   variables({
      :cardProvisioning_sqs => node[:cardProvisioning][:sqs],
-     :cardProvisioning_rds => node[:cardProvisioning][:rds]
+     :cardProvisioning_rds => node[:cardProvisioning][:rds],
+     :cardProvisioning_rds_uid => node[:cardProvisioning][:rds_uid],
+     :cardProvisioning_rds_pwd => node[:cardProvisioning][:rds_pwd]
   })
   notifies :restart, 'service[tomcat7]'
 end
